0ca6a6eefbe23ef2885ad5a87af35c39abc42dba
Michael C. Lau <mikelaujr@gmail.com>
2015-08-09 23:50:43 -0700

    update folder

--- email.rb
+++ email.rb
@@ -8,16 +8,17 @@ end
 
 post '/contact/?' do
   options = {
-  :to => 'to@gmail.com',
+  :to => 'michaelau_1@hotmail.com',
+  :from => params[:email],
   :subject => params[:name] + " has contacted you about a consultation",
   :body => "hi my name is " + params[:name] + "," + "\n\n my phone number is " + params[:phone] + "\n\n my email is " + params[:email] + "," + "\n\n i want to talk about " + params[:message],
   :via => :smtp,
   :via_options => {
-    :address => 'smtp.gmail.com',
+    :address => 'smtp.live.com',
     :port => '587',
     :domain => 'localhost.localdomain',
-    :user_name => 'username@gmail.com',
-    :password => 'password',
+    :user_name => 'michaelau_1@hotmail.com',
+    :password => '473503a',
     :authentication => :plain,
     :enable_starttls_auto => true
     }
